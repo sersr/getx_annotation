f4a6cbaf265d9d131c291831ff7cd2a47546a196
sersr <1334349436@qq.com>
2022-06-07 16:25:25 +0800

    getx_gen 注解

--- lib/src/page_annotation.dart
+++ lib/src/page_annotation.dart
@@ -1,1 +1,12 @@
-
+class GetxPage {
+  const GetxPage({
+    this.name = '',
+    required this.page,
+    this.bindings = const [],
+    this.children = const [],
+  });
+  final String name;
+  final List<Type> bindings;
+  final Type page;
+  final List<GetxPage> children;
+}
